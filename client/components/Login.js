d38e9c25d39a0a04a57371f8e05ff01c84305fad
Ibrahim Shaikh <ibrahims123457@yahoo.com>
2018-04-29 15:56:37 -0400

    Finish login page

--- client/components/Login.js
+++ client/components/Login.js
@@ -27,27 +27,39 @@ class Login extends Component {
     return (
       <div>
         <main role="main" className="container">
-          <div className="jumbotron">
-            <h1>Twitter Username:</h1>
-            <form className="form-inline" onSubmit={this.handleSubmit}>
-              <input className="form-control" aria-label="Username" onChange={this.handleChange}></input>
-              <button className="btn btn-lg btn-primary" display="inline">Search</button>
+
+          <div className="text-center" style={{paddingTop: 20}}>
+            <form className="form-signin">
+              <h1 className="h3 mb-3 font-weight-normal">Please Sign In</h1>
+
+              <button className="btn btn-lg btn-primary btn-block" type="submit">Twitter Login</button>
             </form>
           </div>
-          <div className="jumbotron">
-            <h1>Results:</h1>
-            <div className="table-responsive">
-              <table className="table">
-                <tbody>
-                {this.state.tweets.map((tweet, i) =>
-                  <tr key={i}>
-                    <td>{tweet}</td>
-                  </tr>
-                )}
-                </tbody>
-              </table>
-            </div>
-          </div>
+
+
+
+
+          {/*<div className="jumbotron">*/}
+            {/*<h1>Twitter Username:</h1>*/}
+            {/*<form className="form-inline" onSubmit={this.handleSubmit}>*/}
+              {/*<input className="form-control" aria-label="Username" onChange={this.handleChange}></input>*/}
+              {/*<button className="btn btn-lg btn-primary" display="inline">Search</button>*/}
+            {/*</form>*/}
+          {/*</div>*/}
+          {/*<div className="jumbotron">*/}
+            {/*<h1>Results:</h1>*/}
+            {/*<div className="table-responsive">*/}
+              {/*<table className="table">*/}
+                {/*<tbody>*/}
+                {/*{this.state.tweets.map((tweet, i) =>*/}
+                  {/*<tr key={i}>*/}
+                    {/*<td>{tweet}</td>*/}
+                  {/*</tr>*/}
+                {/*)}*/}
+                {/*</tbody>*/}
+              {/*</table>*/}
+            {/*</div>*/}
+          {/*</div>*/}
         </main>
       </div>
     );
