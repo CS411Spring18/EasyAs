e123cd179faaa2b6b51fb947676642fc6513d5d3
Jacob Kozol <jacobdkozol@gmail.com>
2018-04-29 20:14:04 -0400

    Clean up twitter/watson calls

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,12 @@
 //server/routes/routes.js
 var express = require('express');
 var request = require("request");
+var axios = require('axios');
 var twitterApi = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
 var router = express.Router();
 var bodyParser = require('body-parser');
 var User = require('../../models/User');
 var bearerToken = require('../../config.js').bearerToken;
-var profile = require('../../profileJacob.json');
 
 router.get('/', function(req, res){
   res.render('index');
@@ -15,18 +15,6 @@ router.get('/', function(req, res){
 router.route('/fetchUser')
 .post(function(req,res) {
   var user = new User();
-  var options = {
-    method: 'GET',
-    url: twitterApi,
-    qs: {
-      "screen_name": req.body.name,
-      "count": 200,
-    },
-    json: true,
-    headers: {
-      "Authorization": "Bearer " + bearerToken
-    }
-  };
 
   user.name = req.body.name;
 
@@ -35,77 +23,89 @@ router.route('/fetchUser')
       res.send(err);
     // If there's no data in the database, make the API call to twitter
     } else if (response == null) {
-      request(options, function (error, response, body) {
-        if (!error) {
-          const formattedTweets = formatTwitterResponse(body);
-          var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
-          var personality_insights = new PersonalityInsightsV3({
-            username: '34442ff0-5fd6-4772-ba9e-9e7a1fe3dad5',
-            password: 'heBS6eLUnyDQ',
-            version_date: '2017-10-13'
-          });
-
-          var params = {
-            content: formattedTweets,
-            content_type: 'application/json',
-            raw_scores: true
-          };
-
-          personality_insights.profile(params, function(error, response) {
-            if (error)
-              console.log('Error:', error);
-            else {
-              var personality = formatWatsonResponse(response);
-              user.tweets = body;
-              user.personality = personality;
-              user.save(function (err) {
-                if (err)
-                  res.send(err);
-                res.send(user.personality);
-              });
-            }
-          });
-          // user.tweets = body;
-          // user.personality = personality;
-          // user.save(function (err) {
-          //   if (err)
-          //     res.send(err);
-          //   res.send(user);
-          // });
+      axios.get(twitterApi, {
+        params: {
+          "screen_name": req.body.name,
+          "count": 200,
+        },
+        headers: {
+          "Authorization": "Bearer " + bearerToken
         }
-        else {
-          res.status(500).json({ error: error });
-        }
+      })
+      .then(response => response.data)
+      .then(twitterData => formatTwitterResponse(twitterData))
+      .then(tweets => {
+        user.tweets = tweets;
+        return getProfile(tweets)
+      })
+      .then(personalityProfile => formatWatsonResponse(personalityProfile))
+      .then(personalityTraits => {
+        user.personality = personalityTraits;
+        user.save(function (err) {
+          if (err)
+            res.send(err);
+          res.send(user.personality);
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({ error: error });
       });
     // If data is found in cache, return it
     } else {
       res.send(response.personality);
     }
-  });
+  })
 });
 
-function fetchPersonality(body) {
-  var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
-  var personality_insights = new PersonalityInsightsV3({
-    username: '34442ff0-5fd6-4772-ba9e-9e7a1fe3dad5',
-    password: 'heBS6eLUnyDQ',
-    version_date: '2017-10-13'
-  });
+// This gets stuck in promise land
+const getPersonality = (username, user) =>
+    axios.get(twitterApi, {
+      params: {
+        "screen_name": username,
+        "count": 200,
+      },
+      headers: {
+        "Authorization": "Bearer " + bearerToken
+      }
+    })
+    .then(response => response.data)
+    .then(twitterData => formatTwitterResponse(twitterData))
+    .then(tweets => {
+      user.tweets = tweets;
+      return getProfile(tweets)
+    })
+    .then(personalityProfile => {
+      user.personality = formatWatsonResponse(personalityProfile)
+      return user;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-  var params = {
-    content: body,
-    content_type: 'application/json',
-    raw_scores: true
-  };
 
-  personality_insights.profile(params, function(error, response) {
-    if (error)
-      console.log('Error:', error);
-    else {
-      formatWatsonResponse(response);
-    }
-  });
-}
+const PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+const personalityInsights = new PersonalityInsightsV3({
+  username: '34442ff0-5fd6-4772-ba9e-9e7a1fe3dad5',
+  password: 'heBS6eLUnyDQ',
+  version_date: '2017-10-13'
+});
+
+const getProfile = (tweets) =>
+  new Promise((resolve, reject) => {
+    var params = {
+      content: tweets,
+      content_type: 'application/json',
+      raw_scores: true
+    };
+
+    return personalityInsights.profile(params, (err, profile) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(profile);
+      }
+    });
+});
 
 function formatTwitterResponse(twitterResponse) {
   var formattedTweets = {
